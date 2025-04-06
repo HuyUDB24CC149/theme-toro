2022b11b5c6d79ed4c5966620c3b445165e0e688
HuyUDB24CC149 <HuyUD.B24CC149@hotmail.com>
2025-04-07 03:13:19 +0700

    Delete inc directory

--- inc/rightbar.blade.php
+++
@@ -1,5 +0,0 @@
-<aside>
-    @foreach($tops as $top)
-        @include("themes::themetoro.inc.rightbar." . $top['template'])
-    @endforeach
-</aside>
--- inc/rightbar/rightbar_text.blade.php
+++
@@ -1,8 +0,0 @@
-<div id="toroflix_genres_widget-3" class="Wdgt widget_categories">
-    <div class="Title">{{$top['label']}}</div>
-    <ul>
-        @foreach($top['data'] as $movie)
-            <li><a href="{{$movie->getUrl()}}">{{$movie->name}}</a> {{$movie->publish_year}}</li>
-        @endforeach
-    </ul>
-</div>
